caa3162e2534571f2b9226e7e60853e49b067e70
David J. Hunter <dhunter@westmont.edu>
2017-05-19 15:10:50 -0700

    Finished converting to c++

--- project0.cpp
+++ project0.cpp
@@ -1,101 +1,82 @@
-//========================================================================
+/*===================================================
 // Skeleton Project for CS-150: 3D Computer Graphics
-//
-// Derived from "Simple GLFW example"
-// Copyright (c) Camilla Löwy <elmindreda@glfw.org>
-//
-// This software is provided 'as-is', without any express or implied
-// warranty. In no event will the authors be held liable for any damages
-// arising from the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-//
-// 1. The origin of this software must not be misrepresented; you must not
-//    claim that you wrote the original software. If you use this software
-//    in a product, an acknowledgment in the product documentation would
-//    be appreciated but is not required.
-//
-// 2. Altered source versions must be plainly marked as such, and must not
-//    be misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source
-//    distribution.
-//
-//========================================================================
+//===================================================*/
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-//we'll get rid of these soon TODO
-#include "linmath.h"
-#include <stdlib.h>
-#include <stdio.h>
+using namespace std;
+
+static const int NUM_TRIANGLES = 1;
 
 static const struct
 {
-    float x, y;
-    float r, g, b;
-} vertices[3] =
+    GLfloat x, y, z; // position in R^3
+    GLfloat r, g, b; // color
+} triangles[3*NUM_TRIANGLES] =
 {
-    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+    { -0.5f, -0.289, 0.0f, 1.0f, 0.0f, 0.0f },
+    {  0.5f, -0.289f, 0.0f, 0.0f, 1.0f, 0.0f },
+    {  0.0f,  0.577f, 0.0f, 0.0f, 0.0f, 1.0f }
 };
 
-static void error_callback(int error, const char* description)
+static void errorCallback(int error, const char* description)
 {
-    fprintf(stderr, "Error: %s\n", description);
+    cerr << "GLFW Error: " << description << endl; 
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-/* Function for reading shaders (www.kronos.org) */
-char* filetobuf(char *file)
+static void readShaderCode(const char *filename, vector<char>& data) 
 {
-    FILE *fptr;
-    long length;
-    char *buf;
+    ifstream infile(filename, ios::binary);
+    if (!infile)
+        cerr << "File not found: " << filename << endl;
+    infile.exceptions(ios::eofbit | ios::failbit | ios::badbit);
+    infile.seekg(0, ios::end);
+    int num_chars = infile.tellg();
+    data.resize(num_chars);
+    infile.seekg(0, ios::beg);
+    infile.read(&data[0], num_chars);
+}
 
-    fptr = fopen(file, "rb");
-    if (!fptr) 
+void compileShader(GLuint shader, const char *filename)
+{
+    vector<char> shaderSource;
+    readShaderCode(filename, shaderSource);
+    const char *shaderText = {&shaderSource[0]};
+    const GLint shaderLength[] = {shaderSource.size()};
+    glShaderSource(shader, 1, (const GLchar**)&shaderText, shaderLength);
+    glCompileShader(shader);
+    GLchar infoLog[8192];
+    GLint success = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) 
     {
-        fprintf(stderr, "File not found: %s \n", file);
-        return NULL;
+        glGetShaderInfoLog(shader, 8192, NULL, infoLog);
+        cerr << "Shader " << filename << " failed to complile." << endl
+             << "Error log: " << infoLog << endl;
     }
-    fseek(fptr, 0, SEEK_END);
-    length = ftell(fptr); 
-    buf = (char*)malloc(length+1); // length of the file plus null terminator
-    fseek(fptr, 0, SEEK_SET); 
-    fread(buf, length, 1, fptr); 
-    fclose(fptr); 
-    buf[length] = 0; // Null terminator 
-
-    return buf;
 }
 
 int main(void)
 {
-    // TODO: get rid of. Just for testing.
-    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.f);
     GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
+    GLuint vertexBuffer, vertexShader, fragmentShader, program;
+    GLint mvpLocation, vposLocation, vcolLocation;
 
-    // Read shader code from files
-    char *vertex_shader_text, *fragment_shader_text;
-    vertex_shader_text = filetobuf("simple.vert");   // Note: we are assuming 
-    fragment_shader_text = filetobuf("simple.frag"); // an in-tree build
-
-    glfwSetErrorCallback(error_callback);
+    glfwSetErrorCallback(errorCallback);
 
     if (!glfwInit())
         exit(EXIT_FAILURE);
@@ -109,104 +90,89 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Don't use old OpenGL
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE); // OSX needs this
 
-    window = glfwCreateWindow(640, 480, "Template Project", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "CS 120 Template Project", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
 
-    glfwSetKeyCallback(window, key_callback);
+    glfwSetKeyCallback(window, keyCallback);
 
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
-    glfwSwapInterval(1);
+    glfwSwapInterval(1); // Framerate matches monitor refresh rate
 
-    GLuint VAO;
+    GLuint VAO; // vertex array object
     glGenVertexArrays(1, &VAO);  
     glBindVertexArray(VAO);
 
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
 
-    // Compile shaders and check for errors
-    GLchar info_log[8192];
-    GLint success;
-
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, (const GLchar**)&vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-    if (!success) 
-    {
-        glGetShaderInfoLog(vertex_shader, 8192, NULL, info_log);
-        fprintf(stderr, "Vertex shader failed to complile. ERROR: \n%s\n\n", info_log);
-    }
-
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, (const GLchar**)&fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-    if (!success) 
-    {
-        glGetShaderInfoLog(fragment_shader, 8192, NULL, info_log);
-        fprintf(stderr, "Fragment shader failed to complile. ERROR: \n%s\n\n", info_log);
-    }
+    // Read shaders from files, compile them, and check for errors
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    compileShader(vertexShader, "simple.vert");   // Here is where we are assuming
+    compileShader(fragmentShader, "simple.frag"); // an in-tree build.
 
     program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
     glLinkProgram(program);
+    GLint success = 0;
     glGetProgramiv(program, GL_LINK_STATUS, &success);
     if (!success) 
     {
-        glGetProgramInfoLog(program, 8192, NULL, info_log);
-        fprintf(stderr, "Shader linking failed. ERROR: \n%s\n\n", info_log);
+        cerr << "ERROR: Shader linking failed." << endl;
         glDeleteProgram(program);
         program = 0u;
     }
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader); 
-    free(vertex_shader_text);
-    free(fragment_shader_text);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader); 
 
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
+    mvpLocation = glGetUniformLocation(program, "MVP");
+    vposLocation = glGetAttribLocation(program, "vPos");
+    vcolLocation = glGetAttribLocation(program, "vCol");
 
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) 0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(vertices[0]), (void*) (sizeof(float) * 2));
+    glEnableVertexAttribArray(vposLocation);
+    glVertexAttribPointer(vposLocation, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(triangles[0]), (GLvoid*) 0);
+    glEnableVertexAttribArray(vcolLocation);
+    glVertexAttribPointer(vcolLocation, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(triangles[0]), (GLvoid*) (sizeof(GLfloat) * 3));
+
+    glEnable(GL_DEPTH_TEST);
+
+    glm::mat4 M, V, P, MVP;
+    glm::vec3 eye = glm::vec3(0.0, 0.0, 5.0);
+    glm::vec3 center = glm::vec3(0.0, 0.0, 0.0);
+    glm::vec3 up = glm::vec3(0.0, 1.0, 0.0);
+    V = glm::lookAt(eye, center, up);
 
     while (!glfwWindowShouldClose(window))
     {
         float ratio;
         int width, height;
-        mat4x4 m, p, mvp;
 
         glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-
         glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        ratio = width / (float) height;
+        P = glm::perspective(70.0f, ratio, 1.0f, 100.0f);
+        M = glm::rotate(glm::mat4(1.0f), (float) glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f)); 
+        MVP = P * V * M;
 
         glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUniformMatrix4fv(mvpLocation, 1, GL_FALSE, glm::value_ptr(MVP));
+        glDrawArrays(GL_TRIANGLES, 0, 3*NUM_TRIANGLES);
 
         // check for OpenGL errors
         GLenum error_code;
         while ((error_code = glGetError()) != GL_NO_ERROR) 
-            fprintf(stderr, "OpenGL error HEX: %x \n", error_code);
+            cerr << "OpenGL error HEX: " << error_code << endl;
 
         glfwSwapBuffers(window);
         glfwPollEvents();
